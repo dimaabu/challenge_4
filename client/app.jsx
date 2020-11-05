8cd5849ad59f30cf3840e8450b2ee3ec604cfc43
dimaabu <deemaabuzahra@outlook.com>
2020-11-05 15:13:37 +0200

    g

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,12 @@
+//we are going to make board using react and inside the board the  matrix
+//the matrix is going to be an array inside array
+let newboardmodel = []
 
+for (let x=0;x<7;x++){
+let newcolumn =[]
+for (let y=0;y<7;y++){
+    newcolumn.push(0)
+}
+newboardmodel.push(newColumn)
 
+}
